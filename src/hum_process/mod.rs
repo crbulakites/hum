27f148f2751bedb6f40a6a3d0f2ddf4e5fec78a4
Connor Bulakites <connor@bulakites.net>
2018-11-15 02:19:38 -0500

    Refactor and comment hum_process

--- src/hum_process/mod.rs
+++ src/hum_process/mod.rs
@@ -20,87 +20,118 @@ mod hum_math;
 mod hum_voice;
 use std::collections::HashMap;
 
+// The number of samples of the waveform per seconds of audio:
 static SAMPLE_RATE: u32 = 44_100;
 
+// Add notes to the audio per instructions in the *.hum file:
 pub fn parse_score(score_contents: String) -> Vec<f32> {
-    let mut tempo: f32 = 60.0;
-    let mut measure: u32 = 0;
-    let mut beats: u32 = 4;
-    let mut cursor: f32 = 0.0;
-    let mut offset: f32 = 0.0;
+    // The following variables may or will change as we iterate through the *.hum file:
+    // -------------------------------------------------------------------------------------------
+    // The current number of beats per second (defaults to 1.0 for a tempo of 60.0):
+    let mut beats_per_second: f32 = 1.0;
+    // The current measure being operated on, indexed from 0:
+    let mut measure_index: u32 = 0;
+    // The number of beats in the current measure (defaults to 4):
+    let mut beats_per_measure: f32 = 4.0;
+    // The duration of the current measure in seconds:
+    let mut measure_duration = beats_per_measure / beats_per_second;
+    // The number of seconds into the track at the start of the current measure:
+    let mut timestamp_at_measure_start: f32 = 0.0;
+    // The number of seconds into the current measure from the start of the current measure:
+    let mut timestamp_offset_in_measure: f32 = 0.0;
+    // The current "instrument" or "sound" for notes being inserted into the track (default sine):
     let mut voice: &str = "sine";
-    let volume: f32 = 0.05; // Please be careful with your ears and speakers :)
-
-    let mut note_frequencies: HashMap<String, f32> = hum_math::piano_key_frequencies("sharps");
-
+    // The current key of the track ("sharps" versus "flats", defaults to sharps):
+    let mut key: &str = "sharps";
+    // The current note/frequency mappings (the HashMap keys vary depending on the musical key):
+    let mut note_frequencies: HashMap<String, f32> = hum_math::piano_key_frequencies(key);
+    // The audio track itself; waveforms will be added to this as the *hum file is parsed:
     let mut track: Vec<f32> = Vec::new();
+    // -------------------------------------------------------------------------------------------
 
+    // Load all of the sentences from the *.hum file into a vector:
     let sentences: Vec<&str> = score_contents.split(".").collect();
 
+    // Carry out the commands of all the sentences in the order that they appear:
     for i in 0..sentences.len() - 1 {
+        // Separate the sentence into its command clause and its value clause:
         let clauses: Vec<&str> = sentences[i].split(":").collect();
         let command = clauses[0];
         let value = clauses[1];
 
-        if command == "Tempo" {
-            tempo = value.parse::<f32>().unwrap();
-        } else if command == "Key" {
-            // Perhaps inefficient if already set to sharps; will refactor later
-            note_frequencies = hum_math::piano_key_frequencies(value);
-        } else if command == "Measure" {
-            measure += 1;
-            offset = 0.0;
-            beats = value.parse::<u32>().unwrap();
-            let beats_per_second = tempo / 60.0;
-            let measure_duration = (beats as f32) / beats_per_second;
-            if measure > 1 {
-                cursor += measure_duration;
+        match command {
+            "Tempo" => {
+                // Beats per second corresponds to tempo (beats per minute) divided by seconds:
+                beats_per_second = value.parse::<f32>().unwrap() / 60.0;
             }
-        } else if command == "Voice" {
-            // On a new voice track, begin writing at the beginning of the measure
-            offset = 0.0;
-            voice = value;
-        } else if command == "#" {
-            // Comment command: do nothing
-        } else {
-            // Any other command: presume that it's a note
-            match note_frequencies.get(command) {
-                Some(note_frequency) => {
-                    // Calculate note duration:
-                    let length_values: Vec<&str> = value.split("/").collect();
-                    let length_numerator: f32 = length_values[0].parse::<f32>().unwrap();
-                    let length_denominator: f32 = length_values[1].parse::<f32>().unwrap();
-                    let note_length_of_measure = length_numerator / length_denominator;
-
-                    let beats_per_second = tempo / 60.0;
-                    let measure_duration = (beats as f32) / beats_per_second;
-
-                    let note_duration = measure_duration * note_length_of_measure;
-
-                    // Get the current note position:
-                    let note_position = cursor + offset;
-
-                    add_note_to_track(
-                        note_position,
-                        note_duration,
-                        note_frequency,
-                        volume,
-                        voice,
-                        &mut track,
-                    );
-
-                    println!(
-                        "Added note {} ({} Hz) at {} seconds.",
-                        command, note_frequency, note_position
-                    );
-
-                    // Update the offset:
-                    offset += note_duration;
+            "Key" => {
+                // Possibly expensive operation, do not repeat if unnecessary:
+                if value != key {
+                    key = value;
+                    note_frequencies = hum_math::piano_key_frequencies(key);
                 }
-                None => {
-                    println!("ERROR: cannot make sense of note {}.", value);
+            }
+            "Measure" => {
+                // Update beats per measure and measure duration:
+                beats_per_measure = value.parse::<f32>().unwrap();
+                measure_duration = beats_per_measure / beats_per_second;
+
+                // Advance timestamp by the length of new measure (disregarding the first measure):
+                if measure_index > 0 {
+                    timestamp_at_measure_start += measure_duration;
                 }
-            };
+
+                // Indicate that a measure was added:
+                measure_index += 1;
+            }
+            "Voice" => {
+                // Always set offset to 0.0 at the beginning of a new voice track:
+                timestamp_offset_in_measure = 0.0;
+                // Save voice for later so that we know what type of waveform to generate:
+                voice = value;
+            }
+            "#" => {
+                // This indicates a comment sentence: don't do anything
+            }
+            // For now, let's assume that any other value is a note
+            _ => {
+                match note_frequencies.get(command) {
+                    // If the note is recognized:
+                    Some(note_frequency) => {
+                        // Calculate note duration:
+                        let length_values: Vec<&str> = value.split("/").collect();
+                        let length_numerator: f32 = length_values[0].parse::<f32>().unwrap();
+                        let length_denominator: f32 = length_values[1].parse::<f32>().unwrap();
+                        let note_length_of_measure = length_numerator / length_denominator;
+                        let note_duration = measure_duration * note_length_of_measure;
+
+                        // Get the current note position in the track in seconds:
+                        let note_position =
+                            timestamp_at_measure_start + timestamp_offset_in_measure;
+
+                        add_note_to_track(
+                            note_position,  // Start position of the note in the track in seconds
+                            note_duration,  // Duration of the note to add in seconds
+                            note_frequency, // Frequency of the note
+                            voice,          // "instrument" or "sound" of the note
+                            &mut track,     // Master audio track to be mutated
+                        );
+
+                        // Provide a little bit of output for the user:
+                        println!(
+                            "Added note {} ({} Hz) at {} seconds.",
+                            command, note_frequency, note_position
+                        );
+
+                        // Update the offset:
+                        timestamp_offset_in_measure += note_duration;
+                    }
+                    // If the note isn't recognized:
+                    None => {
+                        println!("ERROR: cannot make sense of note {}.", value);
+                    }
+                };
+            }
         }
     }
 
@@ -108,12 +139,11 @@ pub fn parse_score(score_contents: String) -> Vec<f32> {
 }
 
 fn add_note_to_track(
-    position: f32,        // The start position of the note in the track in seconds
-    duration: f32,        // The duration of the note to add in seconds
-    frequency: &f32,      // The frequency of the note
-    volume: f32,          // The volume/amplitude of the note
-    voice: &str,          // The "instrument" or "sound" of the note
-    track: &mut Vec<f32>, // The master audio track to be mutated
+    position: f32,        // Start position of the note in the track in seconds
+    duration: f32,        // Duration of the note to add in seconds
+    frequency: &f32,      // Frequency of the note
+    voice: &str,          // "instrument" or "sound" of the note
+    track: &mut Vec<f32>, // Master audio track to be mutated
 ) {
     // Generate the appropriate waveform for the note:
     let note = match voice {
@@ -136,6 +166,9 @@ fn add_note_to_track(
             track.extend(vec![0.0; num_samples_to_add]);
         }
     }
+
+    // Please be careful with your ears and speakers! :)
+    let volume = 0.05;
 
     // Add the waveform to the waveforms already present in the master track:
     for i in 0..sample_duration {
