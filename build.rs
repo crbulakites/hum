4ff3a05a4993034f631eeff14158226d8e00f688
Connor Bulakites <connor@bulakites.net>
2023-09-17 00:40:31 -0400

    Remove vcpkg since there is no longer a need to build portaudio on Windows

--- build.rs
+++ build.rs
@@ -1,10 +1,2 @@
-#[cfg(windows)]
-extern crate vcpkg;
-
 fn main() {
-    #[cfg(windows)]
-    {
-        std::env::set_var("VCPKGRS_DYNAMIC", "1");
-        vcpkg::find_package("portaudio").unwrap();
-    }
 }
